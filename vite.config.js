afed0a5b65531b33cecbc71fcc4cf36986ca482a
aftab <shaikaftab>
2024-10-14 15:26:35 +0530

    yoyo

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import dotenv from 'dotenv';  
 
-// Load environment variables from .env file
-
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/repository-name/', // Add this line
   plugins: [react()],
 });
